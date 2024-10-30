6648358561303ce849bcaed3ec9dbfebd49b07b5
Natália Molini Buffa <nati.buffa1907@gmail.com>
2024-10-30 12:03:48 -0300

    main, csv e report

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
 import CSV.LeitorCSV;
+import TabelasHash.HashTableFunctionA;
+import TabelasHash.HashTableFunctionB;
 
 public class Main {
     public static void main(String[] args) {
-        //Lendo arquivo csv names_5000.csv
         String caminho = "names_5000.csv";
         LeitorCSV leitor = new LeitorCSV(caminho);
+        int quantidadeParaInserir = 500;
+        int quantidadeParaBuscar = 100;
+        String[] valoresParaInserir = new String[quantidadeParaInserir];
+        for (int i = 0; i < quantidadeParaInserir; i++) {
+            valoresParaInserir[i] = leitor.getNomes()[i];
+        }
+        String[] valoresParaBuscar = new String[quantidadeParaBuscar];
+        int index = (int) (Math.random() * (4999-quantidadeParaBuscar));
+        for (int i = 0; i < quantidadeParaBuscar; i++) {
+            valoresParaBuscar[i] = leitor.getNomes()[(index + i) % 5000];
+        }
+        int size = quantidadeParaInserir * 2;
+        HashTableFunctionA tabelaA = new HashTableFunctionA(size);
+        tabelaA.insertAll(valoresParaInserir);
+        tabelaA.searchAll(valoresParaBuscar);
+        System.out.println("Relatório da Tabela A:");
+        tabelaA.printReport();
 
+        HashTableFunctionB tabelaB = new HashTableFunctionB(size);
+        tabelaB.insertAll(valoresParaInserir);
+        tabelaB.searchAll(valoresParaBuscar);
+        System.out.println("Relatório da Tabela B:");
+        tabelaB.printReport();
+
+        tabelaA.compare(tabelaB,"Tabela A","Tabela B");
     }
 }
---
+++ src/Relatorio/ReportManager.java
@@ -0,0 +1,56 @@
+package Relatorio;
+
+public abstract class ReportManager {
+    private long insertionStartTime;
+    private long insertionEndTime;
+    private long insertionDelta;
+    private long searchStartTime;
+    private long searchEndTime;
+    private long searchDelta;
+    protected int collisions;
+    public void startInsertion(){
+        insertionStartTime = System.nanoTime();
+    }
+    public void endInsertion(){
+        insertionEndTime = System.nanoTime();
+        insertionDelta = insertionEndTime - insertionStartTime;
+    }
+    public void incrementCollisions(){
+        collisions++;
+    }
+    public void startSearch(){
+        searchStartTime = System.nanoTime();
+    }
+    public void endSearch(){
+        searchEndTime = System.nanoTime();
+        searchDelta = searchEndTime - searchStartTime;
+    }
+    public void printReport(){
+        System.out.println("Tempo de inserção: " + insertionDelta + "ns");
+        System.out.println("Tempo de busca: " + searchDelta + "ns");
+        System.out.println("Colisões: " + collisions);
+        keysPerHash();
+    }
+
+    public abstract void keysPerHash();
+
+    //Comparando duas tabelas, e exibindo a tabela X é mais rápida em tantos numeros
+    public void compare(ReportManager other, String name, String otherName){
+        System.out.println("Comparação entre " + name + " e " + otherName);
+        if (insertionDelta < other.insertionDelta){
+            System.out.println(name + " é mais rápido em " + (other.insertionDelta - insertionDelta) + "ns na inserção");
+        } else {
+            System.out.println(otherName + " é mais rápido em " + (insertionDelta - other.insertionDelta) + "ns na inserção");
+        }
+        if (searchDelta < other.searchDelta){
+            System.out.println(name + " é mais rápido em " + (other.searchDelta - searchDelta) + "ns na busca");
+        } else {
+            System.out.println(otherName + " é mais rápido em " + (searchDelta - other.searchDelta) + "ns na busca");
+        }
+        if (collisions < other.collisions){
+            System.out.println(name + " tem " + (other.collisions - collisions) + " colisões a menos");
+        } else {
+            System.out.println(otherName + " tem " + (collisions - other.collisions) + " colisões a menos");
+        }
+    }
+}
